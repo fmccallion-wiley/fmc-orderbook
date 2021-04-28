2de4c7985078979661b0e6d344dd8bb6dbbb83d2
Steve Shilling <stevshil@users.noreply.github.com>
2021-04-28 17:39:12 +0100

    Single schema file with data types

--- compose/schema.sql
+++ compose/schema.sql
@@ -1,1 +1,29 @@
-schema.sql-wouldlike
+create database orderbook;
+
+use orderbook;
+
+-- Application create portfolio and user tables
+
+create table `user` (
+  userid varchar(255) primary key
+);
+
+create table portfolio (
+  portfolioid bigint primary key,
+  qty int null,
+  symbol varchar(6) null,
+  userid varchar(255) null
+  -- foreign key(userid) references user(userid)
+);
+
+create table `order` (
+  orderid varchar(255) primary key,
+  symbol varchar(6),
+  `type` varchar(10),
+  price numeric(8,2),
+  qty int,
+  userid varchar(255),
+  portfolioid bigint
+  -- foreign key(portfolioid) references portfolio(portfolioid),
+  -- foreign key(userid) references user(userid)
+);
