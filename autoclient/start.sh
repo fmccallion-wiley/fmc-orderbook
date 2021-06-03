3e487332d38d297234c5b942e64349de1312be34
Steve Shilling <stevshil@users.noreply.github.com>
2021-06-03 15:35:50 +0100

    Autoclient update for day 4 lab1

--- autoclient/start.sh
+++ autoclient/start.sh
@@ -4,6 +4,14 @@ while :
 do
   ((mins = 60 * DELAYMINS))
   mins=$(echo "" | awk -v seed="$RANDOM" -v mins="$mins" 'BEGIN{srand(seed)}{print int(rand()*mins)}')
+  numconn=$(echo "" | awk -v seed="$RANDOM" -v mins="$mins" 'BEGIN{srand(seed)}{print int(rand()*1000)}')
+  x=0
+  curl $appname/order66 >/dev/null 2>&1
+  while (( x < numconn ))
+  do
+    curl $appname/history >/dev/null 2>&1 &
+    ((x=x+1))
+  done
   /app/autoclient.py
   echo "Sleeping $mins"
   sleep $mins
